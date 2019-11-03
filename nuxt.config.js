f077151408a6f0fdc18b38950592130f693c4294
keinuma <nununu.mono@gmail.com>
2019-11-03 11:16:19 +0900

    Add eslint setting for nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,18 @@
 export default {
   mode: 'spa',
-  buildModules: ['@nuxt/typescript-build']
+  modules: ['@nuxt/typescript-build'],
+  plugins: ['~/plugins/composition-api'],
+  build: {
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
 }
