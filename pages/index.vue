b357bbd057c8ddf4416b0172a637edcce47ad887
keinuma <nununu.mono@gmail.com>
2019-11-03 10:54:49 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    Hello World! {{ count }}
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface Data {
+  count: number;
+}
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      count: 1
+    }
+  }
+})
+</script>
