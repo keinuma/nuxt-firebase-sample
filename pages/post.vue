46a5b67e05205ea8d3a616d7d8a7ea41a5a670f8
keinuma <nununu.mono@gmail.com>
2019-11-03 13:51:27 +0900

    Add post article page

---
+++ pages/post.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            記事投稿
+          </h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <button class="button is-success">
+          投稿
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import { Article } from '~/types/article'
+
+interface Data {
+  articles: Article[];
+}
+
+export default createComponent({
+  layout: 'default',
+  setup () : Data {
+    return {
+      articles: []
+    }
+  }
+})
+</script>
