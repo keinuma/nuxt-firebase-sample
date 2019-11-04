57d3a516a4497862fec2ead8cce55b10ffce4e6f
keinuma <nununu.mono@gmail.com>
2019-11-04 18:10:41 +0900

    Add complete message to save post

--- pages/post.vue
+++ pages/post.vue
@@ -14,9 +14,26 @@
         <p v-if="state.error.code" class="has-text-danger">
           {{ state.error.message }}
         </p>
-        <button class="button is-success" @click="onPostArticle">
-          投稿
-        </button>
+        <p v-if="state.message" class="has-text-info">
+          {{ state.message }}
+        </p>
+        <div class="field">
+          <label class="label">
+            本文
+          </label>
+          <div class="control">
+            <textarea v-model="state.article.content" class="textarea" rows="10">
+              ブログ本文
+            </textarea>
+          </div>
+        </div>
+        <div class="field">
+          <div class="text-center mt-spacer-m">
+            <button class="button is-success" @click="onPostArticle">
+              投稿
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -28,23 +45,27 @@ import dayjs from 'dayjs'
 import { FirebaseError } from 'firebase'
 import firebase from '@/plugins/firebase'
 import { Article } from '~/types/article'
+import useUserState from '@/services/use-user-state'
 
 interface State {
-  article: Article;
+  article: Article
+  message: string
   error: FirebaseError
 }
 
 export default createComponent({
   layout: 'default',
   setup () {
+    const { author } = useUserState()
     const state = reactive<State>({
+      message: '',
       article: {
-        content: 'テスト',
+        content: '',
         createdAt: dayjs().toISOString(),
         author: {
           id: '',
-          name: 'ぬま',
-          account: 'keinuma',
+          name: '',
+          account: '',
           image: ''
         }
       },
@@ -56,11 +77,17 @@ export default createComponent({
     })
     const onPostArticle = async () => {
       const db = firebase.firestore()
-      await db.collection('articles').add({
-        ...state.article
+      const result = await db.collection('articles').add({
+        ...state.article,
+        createdAt: dayjs().toISOString(),
+        author: author.value || state.article.author
       }).catch((err: FirebaseError) => {
         state.error = err
       })
+      if (result !== void 0) {
+        state.article.content = ''
+        state.message = '記事が保存されました。'
+      }
     }
     return {
       state,
